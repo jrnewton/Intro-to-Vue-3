6f02fb23210263d466de53d8656e230f1d6593c8
Jon Newton <newt@hey.com>
2020-10-30 12:39:39 -0400

    Add bootstrap, display lists

--- main.js
+++ main.js
@@ -8,7 +8,12 @@ const optionsObject = {
       image: './assets/images/socks_green.jpg', 
       url: 'https://www.vuejs.org',
       inventory: 100,
-      onSale: true
+      onSale: true,
+      details: ['50% cotton', '30% wool', '20% poly-blend'],
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue'  }
+      ]
     }
   }
 };
