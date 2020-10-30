e8534927b5580ece19e6db1614da973c81dd6c37
Jon Newton <newt@hey.com>
2020-10-30 12:52:07 -0400

    add cart and event listener

--- main.js
+++ main.js
@@ -3,18 +3,24 @@
 const optionsObject = { 
   data: function() {
     return {
-      product: 'Socks', 
+      cart: 0,
+      product: 'Socks',
       description: 'Soft and cool', 
       image: './assets/images/socks_green.jpg', 
       url: 'https://www.vuejs.org',
       inventory: 100,
-      onSale: true,
+      onSale: false,
       details: ['50% cotton', '30% wool', '20% poly-blend'],
       variants: [
         { id: 2234, color: 'green' },
         { id: 2235, color: 'blue'  }
       ], 
       sizes: [ 8, 9, 10, 11 ]
+    }
+  }, 
+  methods: { 
+    addToCard: function() { 
+      this.cart += 1;
     }
   }
 };
