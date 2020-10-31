36849da4307bf98cfc357d0c10406b063f9bf142
Jon Newton <newt@hey.com>
2020-10-30 20:18:29 -0400

    Track quantity per variant.
    Disable remove from cart and other code that doesn't fit with the new model which is specific to variant. .

--- main.js
+++ main.js
@@ -1,60 +1,65 @@
-//const product = 'Socks'
-
-const optionsObject = { 
+const optionsObject = {
   data: function() {
     return {
       cart: 0,
       product: 'Socks',
       description: 'Soft and cool', 
-      image: './assets/images/socks_green.jpg', 
+      selectedVariant: 0,
       url: 'https://www.vuejs.org',
-      inventory: 15,
       onSale: false,
       details: ['50% cotton', '30% wool', '20% poly-blend'],
       brand: 'Vue Mastery',
       variants: [
         { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
         { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0  }
-      ], 
+      ],
       sizes: [ 8, 9, 10, 11 ]
     }
   }, 
   methods: { 
-    // inStock: function() { 
-    //   return (this.inventory > 0);
-    // },
-
-    // inventoryStyle: function() { 
-    //   if (this.inStock()) { 
-    //     return [ 'button' ];
-    //   }
-    //   else { 
-    //     return [ 'button', 'disabledButton' ];
-    //   }
-    // }, 
-
     addToCart: function() { 
-      if (this.inventory > 0) { 
-        this.inventory -= 1;
-        this.cart += 1;
-      }
+      this.cart += 1;
+      this.currentVariant.quantity -= 1;
     }, 
 
     removeFromCart: function() { 
-      if (this.cart > 0) {
-        this.inventory += 1; 
-        this.cart -= 1;
-      }
+      this.cart -= 1;
+      this.currentVariant.quantity += 1;
     }, 
 
-    switchImage(image) { 
-      this.image = image;
+    updateVariant(index) {
+      this.selectedVariant = index;
     }
   }, 
   computed: {
     title: function() { 
       return this.brand + ' ' + this.product + '!';
+    },
+
+    currentVariant: function() { 
+        return this.variants[this.selectedVariant];
+    }, 
+
+    currentQuantity: function() { 
+      return this.currentVariant.quantity;
+    },
+    
+    image: function() { 
+      return this.currentVariant.image;
+    },
+
+    inStock: function() { 
+      return this.currentQuantity > 0;
+    },
+
+    lowStock: function() {
+      return this.currentQuantity > 0 && this.currentQuantity < 10;
+    },
+
+    noStock: function() { 
+      return this.currentQuantity <= 0;
     }
+
   }
 };
 
