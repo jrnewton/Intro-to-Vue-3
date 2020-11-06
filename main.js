5d5388fbe0c4c93040a52abba7766e2c04d8845e
Jon Newton <newt@hey.com>
2020-11-06 09:45:09 -0500

    Commit about PR

--- main.js
+++ main.js
@@ -13,19 +13,7 @@ const optionsObject = {
     }, 
 
     removeFromCart: function(id) {
-      //TODO: bug this will find first index of id
-      //and 'slice' will cut it *and* the remaining elements.
-      //If your cart looks like this: [green,green,blue,green]
-      //And you have 'green' selected and hit remove, then 
-      //first 'green' will be removed and all following items
-      //and viola your  cart is empty.
-      
-      //const index = this.cart.indexOf(id);
-      //if (index != -1) {
-      //  this.cart.splice(index);
-      //}
-
-      //Better:
+      //See https://github.com/Code-Pop/Intro-to-Vue-3/pull/12
       const lastIndex = this.cart.lastIndexOf(id);
       if (lastIndex != -1) {
         this.cart = this.cart.filter((value,index) => {
